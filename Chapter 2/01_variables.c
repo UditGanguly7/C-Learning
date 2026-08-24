660a10e5e98ec611a62235dabff0485d4df9fd9f
uditganguly <neelchakraborty513@gmail.com>
2026-08-25 00:43:13 +0530

    Added more C practice programs - variables, type_conversion, associativity, typecasting

---
+++ Chapter 2/01_variables.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main() 
+{            
+    int i = 10;  // Declare and initialize 'i' with 10
+    int j = 1;
+    int a = 2, b = 3, c = 4, d = 5;  
+    // %d is called format specifier
+    // %d is for int, %f is for float and c% is for char
+    printf("The value of i is %d and value of j is %d", i, j );
+    printf("The value of i is %d and value of j is %d", a, b );
+    printf("The value of i is %d and value of j is %d", c, d );
+
+    return 0; 
+}
